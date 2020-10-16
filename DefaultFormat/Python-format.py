f58b509f62be7c0133945d4f3cc167c737ebaaa3
lala74 <laminhduc0704@gmail.com>
2020-10-16 14:20:57 +0200

    Change default format directory

---
+++ DefaultFormat/Python-format.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+import sys
+import os
+import getpass
+import hashlib
+import subprocess
+
+def main(argv):
+    test = True
+    testString = "False"
+    test = (testString == "True")
+    print("test: ", test)
+    print("Duc")
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
