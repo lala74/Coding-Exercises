429dbcdd93ed608173951535ca4b517244fc08b0
lala74 <laminhduc0704@gmail.com>
2020-11-13 14:12:13 +0100

    C++ format: add time

--- DefaultFormat/main.cpp
+++ DefaultFormat/main.cpp
@@ -1,3 +1,5 @@
+#include <time.h>
+
 #include <algorithm>
 #include <iostream>
 #include <sstream>
@@ -7,6 +9,8 @@ using namespace std;
 
 int main(int argc, char** argv)
 {
+    clock_t tStart = clock();
+
     // Variable
     string result;
 
@@ -17,5 +21,6 @@ int main(int argc, char** argv)
     // Output
     cout << result << endl;
 
+    printf("Time taken: %.2fs\n", (double)(clock() - tStart) / CLOCKS_PER_SEC);
     return 0;
 }
