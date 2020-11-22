5e7e786d9afd22c43ea7840fff15708b4294a01a
lala74 <laminhduc0704@gmail.com>
2020-11-22 19:57:34 +0100

    SumSquare: updated

--- Divers/SumSquare/C++/main.cpp
+++ Divers/SumSquare/C++/main.cpp
@@ -13,30 +13,39 @@ uint cSum = 9;
 const uint cMin = 0;
 uint cMax = cSum;
 
-uint a, b, c;
-uint d, e, f;
-uint g, h, i;
+uint square[cSquareSize][cSquareSize] = {{3, 3, 3}, {3, 3, 3}, {3,3,3}};
 
 bool is_valid()
 {
-    if((a + b + c) != cSum) return false;
-    if((d + e + f) != cSum) return false;
-    if((g + h + i) != cSum) return false;
-    if((a + d + g) != cSum) return false;
-    if((b + e + h) != cSum) return false;
-    if((c + f + i) != cSum) return false;
-    if((a + e + i) != cSum) return false;
-    if((c + e + g) != cSum) return false;
+    uint sumRows[cSquareSize] = {0};
+    uint sumCols[cSquareSize] = {0};
+    for(uint row = 0; row < cSquareSize; ++row) {
+        for(uint col = 0; col < cSquareSize; ++col) {
+            uint value = square[row][col];
+            sumRows[row] += value;
+            sumCols[col] += value;
 
+            if(row == cSquareSize - 1) {
+                if(sumCols[col] != cSum) return false;
+            }
+            if(col == cSquareSize - 1) {
+                if(sumRows[row] != cSum) return false;
+            }
+        }
+    }
     return true;
 }
 
 void print_result()
 {
     cout << "------------" << endl;
-    cout << a << " " << b << " " << c << endl;
-    cout << d << " " << e << " " << f << endl;
-    cout << g << " " << h << " " << i << endl;
+    for(uint row = 0; row < cSquareSize; ++row) {
+        for(uint col = 0; col < cSquareSize; ++col) {
+            uint value = square[row][col];
+            cout << value << " ";
+        }
+        cout << '\n';
+    }
 }
 
 int main(int argc, char** argv)
@@ -47,32 +56,14 @@ int main(int argc, char** argv)
     uint countResult = 0;
     uint countLoop = 0;
     // Main
-    if (argc == 2){
+    if(argc == 2) {
         string str(argv[1]);
         cSum = stoi(str);
         cMax = cSum;
     }
 
-    for(a = cMin; a <= cMax; ++a) {
-        for(b = cMin; b <= cMax - a; ++b) {                                 // limit max b
-            c = cSum - a - b;                                               // calculate c from a and b
-            for(d = cMin; d <= cMax - a; ++d) {                             // limit max d
-                uint maxE = (cMax - d) < (cMax - b) ? cMax - d : cMax - b;  // choose to limit max e by d or b
-                for(e = cMin; e <= maxE; ++e) {
-                    f = cSum - d - e;
-                    // Calculate g h i
-                    g = cSum - a - d;
-                    h = cSum - b - e;
-                    i = cSum - c - f;
-
-                    if(is_valid()) {
-                        ++countResult;
-                        print_result();
-                    }
-                    ++countLoop;
-                }
-            }
-        }
+    if (is_valid()) {
+        print_result();
     }
 
     // Output
