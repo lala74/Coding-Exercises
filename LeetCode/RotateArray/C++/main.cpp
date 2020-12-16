407002123d02e65b79b033e540e2917d11eb76a9
lala74 <laminhduc0704@gmail.com>
2020-12-16 15:02:50 +0100

    RotateArray: 2nd solution

--- LeetCode/RotateArray/C++/main.cpp
+++ LeetCode/RotateArray/C++/main.cpp
@@ -15,15 +15,31 @@ using namespace std;
 class Solution
 {
 public:
+    // 1st solution
+    // void rotate(vector<int>& nums, int k)
+    // {
+    //     int len = static_cast<int>(nums.size());
+    //     int arr[len];
+    //     for(int i = 0; i < len; i++) {
+    //         arr[i] = nums[i];
+    //     }
+    //     for(int i = 0; i < len; i++) {
+    //         nums[(i + k) % len] = arr[i];
+    //     }
+    // }
+
+    // 2nd solution
     void rotate(vector<int>& nums, int k)
     {
         int len = static_cast<int>(nums.size());
-        int arr[len];
-        for(int i = 0; i < len; i++) {
-            arr[i] = nums[i];
-        }
-        for(int i = 0; i < len; i++) {
-            nums[(i + k) % len] = arr[i];
+        k %= len;
+        for(int rotation = 0; rotation < k; ++rotation) {
+            int previous = nums[len - 1];
+            for(int i = 0; i < len; ++i) {
+                int temp = nums[i];
+                nums[i] = previous;
+                previous = temp;
+            }
         }
     }
 };
@@ -53,6 +69,6 @@ int main(int argc, char** argv)
     printf("Time taken  : %.2f s\n", (double)(clock() - tStart) / CLOCKS_PER_SEC);
     // Memory usage
     look_up_our_self(&usage);
-    printf("Memory usage: %.2f MB\n", static_cast<float>(usage.vsize) / 1024 / 1024);
+    printf("Memory usage: %.3f MB\n", static_cast<float>(usage.vsize) / 1024 / 1024);
     return 0;
 }
