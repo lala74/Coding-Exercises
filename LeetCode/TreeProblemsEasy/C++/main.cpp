1da6cf54473b79c7c933002787170a9c0c97ab90
lala74 <laminhduc0704@gmail.com>
2020-12-28 13:40:12 +0100

    ValidBST: 2nd solution

--- LeetCode/TreeProblemsEasy/C++/main.cpp
+++ LeetCode/TreeProblemsEasy/C++/main.cpp
@@ -1,3 +1,4 @@
+#include <limits.h>
 #include <proc/readproc.h>
 #include <time.h>
 
@@ -33,52 +34,70 @@ public:
         if(root->right == nullptr) return maxDepth(root->left) + 1;
         return max(maxDepth(root->left) + 1, maxDepth(root->right) + 1);
     }
-//     // 1st solution 
-//     bool isValidBST(TreeNode* root)
-//     {
-//         if(root == nullptr) return true;
-//         int minSub = root->val;
-//         int maxSub = root->val;
-//         return is_valid_BST(root, &minSub, &maxSub);
-//     }
+    //     // 1st solution
+    //     bool isValidBST(TreeNode* root)
+    //     {
+    //         if(root == nullptr) return true;
+    //         int minSub = root->val;
+    //         int maxSub = root->val;
+    //         return is_valid_BST(root, &minSub, &maxSub);
+    //     }
 
-// private:
-//     bool is_valid_BST(TreeNode* root, int* minSub, int* maxSub)
-//     {
-//         if((root->left == nullptr) && (root->right == nullptr)) {
-//             *minSub = root->val;
-//             *maxSub = root->val;
-//             return true;
-//         }
-//         if(root->left == nullptr) {
-//             if(!is_valid_BST(root->right, minSub, maxSub) || (*minSub <= root->val)) {
-//                 return false;
-//             }
-//             *minSub = root->val;
-//             return true;
-//         }
-//         if(root->right == nullptr) {
-//             if(!is_valid_BST(root->left, minSub, maxSub) || (*maxSub >= root->val)) {
-//                 return false;
-//             }
-//             *maxSub = root->val;
-//             return true;
-//         }
-//         int min = 0, max = 0;
-//         if(is_valid_BST(root->left, minSub, maxSub) && (*maxSub < root->val)) {
-//             min = *minSub;
-//         } else {
-//             return false;
-//         }
-//         if(is_valid_BST(root->right, minSub, maxSub) && (*minSub > root->val)) {
-//             max = *maxSub;
-//         } else {
-//             return false;
-//         }
-//         *minSub = min;
-//         *maxSub = max;
-//         return true;
-//     }
+    // private:
+    //     bool is_valid_BST(TreeNode* root, int* minSub, int* maxSub)
+    //     {
+    //         if((root->left == nullptr) && (root->right == nullptr)) {
+    //             *minSub = root->val;
+    //             *maxSub = root->val;
+    //             return true;
+    //         }
+    //         if(root->left == nullptr) {
+    //             if(!is_valid_BST(root->right, minSub, maxSub) || (*minSub <= root->val)) {
+    //                 return false;
+    //             }
+    //             *minSub = root->val;
+    //             return true;
+    //         }
+    //         if(root->right == nullptr) {
+    //             if(!is_valid_BST(root->left, minSub, maxSub) || (*maxSub >= root->val)) {
+    //                 return false;
+    //             }
+    //             *maxSub = root->val;
+    //             return true;
+    //         }
+    //         int min = 0, max = 0;
+    //         if(is_valid_BST(root->left, minSub, maxSub) && (*maxSub < root->val)) {
+    //             min = *minSub;
+    //         } else {
+    //             return false;
+    //         }
+    //         if(is_valid_BST(root->right, minSub, maxSub) && (*minSub > root->val)) {
+    //             max = *maxSub;
+    //         } else {
+    //             return false;
+    //         }
+    //         *minSub = min;
+    //         *maxSub = max;
+    //         return true;
+    //     }
+//  2nd solution
+    bool isValidBST(TreeNode* root)
+    {
+        return is_valid_BST(root, nullptr, nullptr);
+    }
+
+private:
+    bool is_valid_BST(TreeNode* root, const int* min, const int* max)
+    {
+        if(root == nullptr) return true;
+        if ((min != nullptr) && (root->val <= *min)) {
+            return false;
+        }
+        if((max != nullptr) && (root->val >= *max)) {
+            return false;
+        }
+        return (is_valid_BST(root->left, min, &root->val) && (is_valid_BST(root->right, &root->val, max)));
+    }
 };
 
 int main(int argc, char** argv)
@@ -91,11 +110,11 @@ int main(int argc, char** argv)
 
     // Input
     TreeNode* left = new TreeNode(15);
-    TreeNode* right = new TreeNode(7);
+    TreeNode* right = new TreeNode(2147483647);
     TreeNode* root = new TreeNode(20, left, right);
     right = root;
-    left = new TreeNode(9);
-    root = new TreeNode(3, left, right);
+    left = new TreeNode(5);
+    root = new TreeNode(9, left, right);
 
     cout << root->val << endl;
     cout << root->left->val << ' ';
