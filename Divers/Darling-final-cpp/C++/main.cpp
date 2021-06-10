3bafbca74f5f12a486995cf57d035ec1ded7dec3
Duc La <dla@axon.com>
2021-06-10 18:35:33 +0700

    Finish menu

--- Divers/Darling-final-cpp/C++/main.cpp
+++ Divers/Darling-final-cpp/C++/main.cpp
@@ -19,7 +19,7 @@ struct contactType {
     string address;
 };
 
-enum returnValue {
+enum returnType {
     OK = 0,
     CONTACT_EXISTED = 1,
     INVALID_INPUT = 2,
@@ -33,15 +33,110 @@ contactType search_contact_in_file(string fileName, string name, string phone);
 int delete_contact_in_file(string fileName, string name, string phone);
 int edit_contact_in_file(string fileName, string name);
 
+// Some small function
+contactType convert_vector_to_contact_type(vector<string> v);
+string convert_vector_to_string(vector<string> v);
+string convert_contact_type_to_string(contactType contact);
+
 // Menu function
 bool verify_choice(char choice, vector<char> options)
 {
-    if (find(options.begin(), options.end(), choice) != options.end()) {
+    if(find(options.begin(), options.end(), choice) != options.end()) {
         return true;
     }
     return false;
 }
-void print_menu(){
+
+void wait_for_enter()
+{
+    cout << '\n';
+    cout << "Press Enter to continue" << endl;
+    fflush(stdin);
+    cin.get();
+}
+
+void menu_add_new_contact()
+{
+    contactType contact;
+    cout << "Please type name: ";
+    fflush(stdin);
+    getline(cin, contact.name);
+    cout << "Please type phone: ";
+    fflush(stdin);
+    getline(cin, contact.phone);
+    cout << "Please type email : ";
+    fflush(stdin);
+    getline(cin, contact.email);
+    cout << "Please type address: ";
+    fflush(stdin);
+    getline(cin, contact.address);
+
+    returnType status = (returnType)add_new_contact_to_file(contactFileName, contact);
+
+    if(status == OK) {
+        cout << "Contact added" << endl;
+    } else {
+        cout << "Contact is not added" << endl;
+    }
+    wait_for_enter();
+}
+
+void menu_list_all_contacts()
+{
+    list_all_contact_in_file(contactFileName);
+    wait_for_enter();
+}
+
+void menu_search_for_contact()
+{
+    string name, phone;
+
+    cout << "Type name or phone number to search" << endl;
+    cout << "Press enter if a field does not important" << endl;
+    cout << "Please type name: ";
+    fflush(stdin);
+    getline(cin, name);
+    cout << "Please type phone: ";
+    fflush(stdin);
+    getline(cin, phone);
+
+    cout << '\n';
+    contactType contactFound = search_contact_in_file(contactFileName, name, phone);
+    if(contactFound.name == "") {
+        cout << "Contact not found !!" << endl;
+    } else {
+        cout << "Contact found !!" << endl;
+        cout << convert_contact_type_to_string(contactFound) << endl;
+    }
+    wait_for_enter();
+}
+
+void menu_edit_contact()
+{
+    string name;
+
+    cout << "Type contact's name to edit" << endl;
+    fflush(stdin);
+    getline(cin, name);
+
+
+    edit_contact_in_file(contactFileName, name);
+    wait_for_enter();
+}
+
+void menu_delete_contact()
+{
+    wait_for_enter();
+}
+
+void clear_screen()
+{
+    cout << string(20, '\n');
+}
+
+void print_menu()
+{
+    cout << string(3, '\n');
     cout << "\e[1mMAIN MENU\e[0m" << endl;
     cout << "==============================" << endl;
     cout << "[1] Add a new contact" << endl;
@@ -49,31 +144,47 @@ void print_menu(){
     cout << "[3] Search for contact" << endl;
     cout << "[4] Edit a contact" << endl;
     cout << "[5] Delete a contact" << endl;
+    cout << "[q] Quit" << endl;
     cout << "==============================" << endl;
 }
+
 void start_menu()
 {
     print_menu();
     bool ok = false;
+    char choice;
     while(!ok) {
         cout << "Enter the choice: ";
-        char choice;
-        vector<char> options = { '1', '2', '3', '4' };
+        vector<char> options = {'1', '2', '3', '4', '5', 'q'};
         cin >> choice;
-        if (verify_choice(choice, options)) {
-            cout << "Right choice" << endl;
+        if(verify_choice(choice, options)) {
             ok = true;
         } else {
             cout << "Wrong choice" << endl;
             ok = false;
         }
     }
-}
 
-// Some small function
-contactType convert_vector_to_contact_type(vector<string> v);
-string convert_vector_to_string(vector<string> v);
-string convert_contact_type_to_string(contactType contact);
+    if(choice == '1') {
+        clear_screen();
+        menu_add_new_contact();
+    } else if(choice == '2') {
+        clear_screen();
+        menu_list_all_contacts();
+    } else if(choice == '3') {
+        clear_screen();
+        menu_search_for_contact();
+    } else if(choice == '4') {
+        clear_screen();
+        menu_edit_contact();
+    } else if(choice == '5') {
+        clear_screen();
+        menu_delete_contact();
+    } else if(choice == 'q') {
+        cout << "Exit !!" << endl;
+        exit(0);
+    }
+}
 
 string data[6][4] = {{"My heo", "01216760247", "giftchanh@gmail.com", "1 Che Lan Vien, Taiwan"},
                      {"Minh Duc", "0777117497", "laminhduc0704@gmail.com", "1 Che Lan Vien, Vietnam"},
@@ -93,10 +204,6 @@ int main()
         contact.address = data[i][3];
         add_new_contact_to_file(contactFileName, contact);
     }
-
-    cout << "************ List all contact" << endl;
-    list_all_contact_in_file(contactFileName);
-
     cout << "************ Search contact in file" << endl;
     cout << convert_contact_type_to_string(search_contact_in_file(contactFileName, "My heo", "01216760247")) << endl;
 
@@ -107,7 +214,9 @@ int main()
     cout << "************ Edit contact in file" << endl;
     /* edit_contact_in_file(contactFileName, "Mon heu"); */
     /* list_all_contact_in_file(contactFileName); */
-    start_menu();
+    while(1) {
+        start_menu();
+    }
 
     return 0;
 }
@@ -117,7 +226,7 @@ int add_new_contact_to_file(string fileName, contactType contact)
     contactType contactFound;
     contactFound = search_contact_in_file(fileName, contact.name, contact.phone);
     if(contactFound.name != "") {
-        cout << "There is already this contact in file" << endl;
+        cout << "Contact existed" << endl;
         cout << convert_contact_type_to_string(contactFound) << endl;
         return CONTACT_EXISTED;
     }
