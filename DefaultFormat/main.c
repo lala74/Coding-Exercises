8d1b15deb76ed581683984bb86a713a9325ae2f5
Duc La <dla@axon.com>
2022-03-26 12:10:39 +0700

    Default format for c

---
+++ DefaultFormat/main.c
@@ -0,0 +1,14 @@
+/**
+ * @author      : La Minh Duc (dla@$HOSTNAME)
+ * @file        : main
+ * @created     : Sunday Mar 13, 2022 15:54:54 +07
+ */
+
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello, world!\n"); 
+    return 0;
+}
+
