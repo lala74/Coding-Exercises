f8f0d1b247a0c4bacc50802d57746a7e1c1ff2f3
Duc La <dla@axon.com>
2022-05-12 22:55:35 +0700

    BinaryTree - Go - Universal Idea

--- LeetCode/BinaryTree/Go/main.go
+++ LeetCode/BinaryTree/Go/main.go
@@ -28,7 +28,7 @@ func (s *Stack) Pop() interface{} {
 	return value
 }
 
-func (s *Stack) Peek() interface{} {
+func (s *Stack) Top() interface{} {
 	if s.IsEmpty() {
 		return nil
 	}
@@ -43,22 +43,13 @@ type TreeNode struct {
 
 func main() {
 	root := &TreeNode{Val: 1}
-	root.Left = &TreeNode{Val: 3}
+	// root.Left = &TreeNode{Val: 3}
 	root.Right = &TreeNode{Val: 2}
-	root.Left.Left = &TreeNode{Val: 4}
-	root.Left.Right = &TreeNode{Val: 5}
+	// root.Left.Left = &TreeNode{Val: 4}
+	// root.Left.Right = &TreeNode{Val: 5}
+	root.Right.Left = &TreeNode{Val: 3}
 	order := postorderTraversal(root)
 	fmt.Println(order)
-	// s := Stack{}
-	// s.Push(1)
-	// s.Push(2)
-	// s.Push(3)
-	// s.Push(4)
-	// s.Push(5)
-	// s.Pop()
-	// s.Pop()
-	// s.Pop()
-	// fmt.Println("len", s.Len())
 }
 
 // POSTORDER
@@ -74,22 +65,22 @@ func postorderIteration(root *TreeNode, result *[]int) {
 		return
 	}
 	s := Stack{}
-	current := root
-	s.Push(current)
+	s.Push(root)
+	// *result = append(*result, root.Val)
+	lastPop := root
 	for s.Len() != 0 {
-		if current != nil {
-			s.Push(current)
-			current = current.Left
+		top := s.Top().(*TreeNode)
+		if top.Left != nil && top.Left != lastPop && top.Right != lastPop { // push left
+			s.Push(top.Left)
 			continue
 		}
-		node := s.Peek().(*TreeNode)
-		current = node.Right
-		if current == nil {
-			s.Pop()
-			*result = append(*result, node.Val)
+		if top.Right != nil && top.Right != lastPop { // push right
+			s.Push(top.Right)
+			continue
 		}
+		lastPop = s.Pop().(*TreeNode)
+		*result = append(*result, lastPop.Val) // visit pop
 	}
-	*result = (*result)[:len(*result)-1]
 }
 
 func postorderRecursion(root *TreeNode, result *[]int) {
@@ -104,26 +95,32 @@ func postorderRecursion(root *TreeNode, result *[]int) {
 // PREORDER
 func preorderTraversal(root *TreeNode) []int {
 	result := []int{}
-	if root == nil {
-		return nil
-	}
 	// inorderRecursion(root, &result)
 	preorderInteration(root, &result)
 	return result
 }
 
 func preorderInteration(root *TreeNode, result *[]int) {
+	if root == nil {
+		return
+	}
 	s := Stack{}
 	s.Push(root)
+	*result = append(*result, root.Val)
+	lastPop := root
 	for s.Len() != 0 {
-		top := s.Pop().(*TreeNode)
-		*result = append(*result, top.Val)
-		if top.Right != nil {
-			s.Push(top.Right)
-		}
-		if top.Left != nil {
+		top := s.Top().(*TreeNode)
+		if top.Left != nil && top.Left != lastPop && top.Right != lastPop { // push left
 			s.Push(top.Left)
+			*result = append(*result, top.Left.Val) // visit left
+			continue
 		}
+		if top.Right != nil && top.Right != lastPop { // push right
+			s.Push(top.Right)
+			*result = append(*result, top.Right.Val) // visit left
+			continue
+		}
+		lastPop = s.Pop().(*TreeNode)
 	}
 }
 
@@ -133,26 +130,35 @@ func inorderTraversal(root *TreeNode) []int {
 	if root == nil {
 		return nil
 	}
-	inorderRecursion(root, &result)
-	// inorderInteration(root, &result)
+	// inorderRecursion(root, &result)
+	inorderInteration(root, &result)
 	return result
 }
 
 func inorderInteration(root *TreeNode, result *[]int) {
+	if root == nil {
+		return
+	}
 	s := Stack{}
-	current := root
-	s.Push(current)
+	s.Push(root)
+	// *result = append(*result, root.Val)
+	lastPop := root
 	for s.Len() != 0 {
-		if current != nil {
-			s.Push(current)
-			current = current.Left
-		} else {
-			node := s.Pop().(*TreeNode)
-			*result = append(*result, node.Val)
-			current = node.Right
+		top := s.Top().(*TreeNode)
+		if top.Left != nil && top.Left != lastPop && top.Right != lastPop { // push left
+			s.Push(top.Left)
+			continue
+		}
+		if top.Right != nil && top.Right != lastPop { // push right
+			s.Push(top.Right)
+			*result = append(*result, top.Val) // visit top
+			continue
+		}
+		lastPop = s.Pop().(*TreeNode)
+		if top.Right == nil {
+			*result = append(*result, top.Val) // visit top
 		}
 	}
-	*result = (*result)[:len(*result)-1]
 }
 
 func inorderRecursion(root *TreeNode, result *[]int) {
